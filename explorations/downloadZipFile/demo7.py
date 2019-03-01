f84d4b074f53f03bc9ee677df9e69b454f584abf
paul-shannon <paul.thurmond.shannon@gmail.com>
2019-03-01 07:39:09 -0800

    still broken.  url formation apparenetly additive

--- explorations/downloadZipFile/demo7.py
+++ explorations/downloadZipFile/demo7.py
@@ -4,6 +4,7 @@ import dash
 import dash_html_components as html
 import dash_core_components as dcc
 from dash.dependencies import Output, Input, State
+import os
 
 buttonStyle = {"margin-left": 30, "margin-top": 20, "font-size": 24};
 
@@ -13,8 +14,8 @@ app.layout = html.Div([
     dcc.Store(id='storage_projectDirectory'),
     dcc.Dropdown(id='projectChooser',
                  options=[{'label': 'Inferno', 'value': 'inferno'}, {'label': 'Daylight', 'value': 'daylight'}],
-                 value='inferno',
-                 style={"width": 100, "margin-left": 30}),
+                 #value='inferno',
+                 style={"width": 100, "margin-left": 30, "width": 200}),
     html.A(html.Button('Download the Inferno Demo', style=buttonStyle), href='infernoDemo.zip'),
     html.Br(),
     html.Button('Pretend to assemble the text', id="assembleTextButton", style=buttonStyle),
@@ -23,9 +24,21 @@ app.layout = html.Div([
            children=[html.Button('Download newly assembled text',
                                  id="downloadAssembledTextButton",
                                  style=buttonStyle,
-                                 disabled="True")],
+                                 disabled="False")],
            href='webpage.zip'),
     ])
+
+# @app.server.route('/PROJECTS/<path:urlpath>')
+# def downloadProjectZipFile(urlpath):
+#    print("--- serve_static_file")
+#    print("urlpath:  %s" % urlpath)
+#    fullPath = os.path.join("PROJECTS", urlpath)
+#    dirname = os.path.dirname(fullPath)
+#    filename = os.path.basename(fullPath)
+#    print("about to send %s, %s" % (dirname, filename))
+#    return flask.send_file(fullPath,
+#                           mimetype='application/zip',
+#                           as_attachment=True)
 
 @app.server.route('/<filename>')
 def downloadZip(filename):
@@ -36,14 +49,15 @@ def downloadZip(filename):
 #----------------------------------------------------------------------------------------------------
 # the assembleTextButton, when clicked, has these consequences:
 #  -  text is assembled (simulated here in a call to a function stub
-#  -  the downlaodAssembledText button is enabled, setting up that action
+#  -  the downloadAssembledText button is enabled, setting up that action
 #----------------------------------------------------------------------------------------------------
 @app.callback(Output('downloadAssembledTextButton', 'disabled'),
               [Input('assembleTextButton', 'n_clicks')],
               [State('storage_projectDirectory', 'data')])
 def set_button_enabled_state(n_clicks, projectDirectory):
+    print("assembleTextButton callback")
     if(n_clicks is None):
-        return(True)
+        return(False)
     assembleText(projectDirectory)
     return False
 
@@ -55,12 +69,17 @@ def set_button_enabled_state(n_clicks, projectDirectory):
     Output('storage_projectDirectory', 'data'),
     [Input('projectChooser', 'value')])
 def updateStorage(value):
+    print("projectChooser new value, updaing storage_projectDirectory: %s" % value)
     return value;
 
 @app.callback(
     Output('downloadURL', 'href'),
-    [Input('storage_projectDirectory', 'data')])
-def updateDownloadURL(data):
+    [Input('storage_projectDirectory', 'modified_timestamp')],
+    [State('storage_projectDirectory', 'data')])
+def updateDownloadURL(modified_timestamp, data):
+    print("storage_projectDirectory input changes")
+    print("    mts: %s" % modified_timestamp)
+    print("   data: %s" % data);
     return ("PROJECTS/%s/webpage.zip" % data)
 
 #----------------------------------------------------------------------------------------------------
