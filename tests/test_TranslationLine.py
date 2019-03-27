8727b28b59bdcb1d759ec5feafe8b8810eaded3c
David Beck <David@iMac-2.local>
2019-03-27 17:22:43 -0600

    all errors caught and tests passed except the apostrophe as part of orthography error. will mull that one over a bit

--- tests/test_TranslationLine.py
+++ tests/test_TranslationLine.py
@@ -8,10 +8,6 @@ from translationLine import *
 import re
 #----------------------------------------------------------------------------------------------------
 def runTests():
-    test_standardizeTranslationLine()
-    
-#----------------------------------------------------------------------------------------------------
-def test_standardizeTranslationLine():
     """
       ensure we can handle all proper and improper translation lines
       the eaf xml schema only requires that they be character strings
@@ -23,7 +19,6 @@ def test_standardizeTranslationLine():
       the improper translation lines we imagine, or encounter over time, to ensure
       that we can render them into our proper form 
     """
-
     from xml.etree import ElementTree as etree
     filename = "../testData/Chatino_FaultyAuthorExamples/20150717_Prayer_community_one.eaf"
     doc = etree.parse(filename)
@@ -32,10 +27,57 @@ def test_standardizeTranslationLine():
     assert(len(childElements) >= 9)
     cases = [element.text for element in childElements]
        # untested regex!
-    regex = re.compile("‘\S.+\S’")   # opening curly single quote, no whitespace, any characters, no whitespace, closing curly single quote
+    #regex = re.compile("‘\S.+\S’")   # opening curly single quote, no whitespace, any characters, no whitespace, closing curly single quote
     for case in cases:
-       translationLine = TranslationLine(case)
-       standardizedLine = translationLine.getStandardized()
-       assert(len(regex.findall(standardizedLine)) == 1)
+        translationLine = TranslationLine(case)
+        standardizedLine = translationLine.getStandardized()
+        print(standardizedLine)
+        test_begins_and_ends_with_squo(standardizedLine)
+        test_extraneous_whitespace(standardizedLine)
+        test_punctuation_outside_quote(standardizedLine)
+        test_straight_apostrophes(standardizedLine)
+        test_straight_dquo(standardizedLine)
     
 #----------------------------------------------------------------------------------------------------
+def test_begins_and_ends_with_squo(standardizedLine):
+    '''makes sure the first and last character of the translation is a single quote'''
+    
+    # untested regex!
+    regex = re.compile("‘\S.+\S’")   # opening curly single quote, no whitespace, any characters, no whitespace, closing curly single quote
+    assert(len(regex.findall(standardizedLine)) == 1)
+    
+#----------------------------------------------------------------------------------------------------
+def test_extraneous_whitespace(standardizedLine):
+    '''checks to make sure author did not have whitespaces at either end of the translation line'''
+    
+    regex = re.compile("‘\s")   # opening curly single quote followed by whitespace
+    assert(len(regex.findall(standardizedLine)) == 0)
+    regex = re.compile("\s’")   # close curly single quote preceded by whitespace
+    assert(len(regex.findall(standardizedLine)) == 0)
+    
+#----------------------------------------------------------------------------------------------------
+def test_punctuation_outside_quote(standardizedLine):
+    '''checks to make sure author did not have a punctuation mark outside a close quote'''
+    
+    regex = re.compile("’[\.,!?\)]’")   # punctuation preceded by rsquo
+    assert(len(regex.findall(standardizedLine)) == 0)
+
+#----------------------------------------------------------------------------------------------------
+def test_straight_apostrophes(standardizedLine):
+    '''checks to make sure author did not use straight apostrophes rather than squo'''
+    
+    regex = re.compile("‘'")   # initial straight apostrophe
+    assert(len(regex.findall(standardizedLine)) == 0)
+    regex = re.compile("'’")   # final straight apostrophe
+    assert(len(regex.findall(standardizedLine)) == 0)
+    
+#----------------------------------------------------------------------------------------------------
+def test_straight_dquo(standardizedLine):
+    '''checks to make sure author did not use straight double quotes'''
+    
+    regex = re.compile('"')   # check for double quotes
+    assert(len(regex.findall(standardizedLine)) == 0)
+    
+#----------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    runTests()
--- translationLine.py
+++ translationLine.py
@@ -7,13 +7,15 @@
 #  are embedded straight single quotes (') permitted?
 #  are embedded double quotes permitted?
 #----------------------------------------------------------------------------------------------------
+import re
+
 class TranslationLine:
 
    rawText = ""
    cleanText = ""
 
    def __init__(self, rawText):
-      self.rawText = rawText
+      self.rawText = rawText.strip()
       self.cleanText = self.standardize()
 
    def standardize(self):
@@ -22,13 +24,28 @@ class TranslationLine:
          string = string[1:]
       if string[-1] == "’":
          string = string[:-1]
+      # check for apostrophes instead of squo
+      if string[0] == "'":
+         string = string[1:]
+      if string[-1] == "'":
+         string = string[:-1]
+      # check for punctuation following rsquo
+      regex = re.compile("’[\.,!?\)]$")
+      if regex.search(string):
+         punctuation = string[-1]
+         string = string[:-2].strip() + punctuation
+      # replace straight double quotes with smart quotes
+      if '"' in string:
+         string = re.sub('^"','“',string)
+         string = re.sub('\s"',' “',string)
+         string = string.replace('"','”')
       string = "‘" + string.strip() + "’"
       return string
 
-    def getRaw(self):
+   def getRaw(self):
       return self.rawText
 
-    def getStandardized(self):
+   def getStandardized(self):
       return self.cleanText
 
   
