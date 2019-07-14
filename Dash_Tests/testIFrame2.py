a9eb20b935934b3415801513fc3ac2f7477caa27
David Beck <dbeck@ualberta.ca>
2019-07-14 16:05:00 -0600

    new test with flask route and button to change iFrame contents added; no joy yet

--- Dash_Tests/testIFrame2.py
+++ Dash_Tests/testIFrame2.py
@@ -1,5 +1,6 @@
 import dash
 import flask
+import os
 from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
@@ -9,8 +10,21 @@ server = flask.Flask(__name__)
 app = dash.Dash(__name__,server=server)
 app.layout = html.Div(children=[
     html.H1(children='Test cases',className="banner"),
-    html.Iframe(id="storyIFrame",         src='Users/David/OpenSource/github/Dash_Tests/Inferno.html',     className="webpageFrame")
+    html.Iframe(id="storyIFrame",
+                src='Story',
+                className="webpageFrame"),
+    html.Button("press here",id="button")
     ])
+
+
+@app.callback(Output("storyIFrame","src"),
+              [Input("button","n_clicks")]
+    )
+def on_Button_Click(n_clicks):
+    if n_clicks is None:
+        return('')
+    print('=== button clicked')
+    return('Inferno.html')
 
 @server.route('/static/<path:path>')
 def serve_static(path):
